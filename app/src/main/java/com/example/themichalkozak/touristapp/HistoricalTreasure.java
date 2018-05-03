51f6ccf26b7461f283a22c5758782154573e4f7c
seeminglySafe <seeminglysafe@gmail.com>
2018-05-03 16:54:44 +0200

    first commit

---
+++ app/src/main/java/com/example/themichalkozak/touristapp/HistoricalTreasure.java
@@ -0,0 +1,13 @@
+package com.example.themichalkozak.touristapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HistoricalTreasure extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_historical_treasure);
+    }
+}
---
+++ app/src/main/java/com/example/themichalkozak/touristapp/ListElements.java
@@ -0,0 +1,10 @@
+package com.example.themichalkozak.touristapp;
+
+/**
+ * Created by themichalkozak on 03/05/2018.
+ */
+
+public class ListElements {
+
+
+}
