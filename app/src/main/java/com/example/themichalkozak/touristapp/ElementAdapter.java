f0a332478c238b4060b221f7aaf12340d63984fd
seeminglySafe <seeminglysafe@gmail.com>
2018-05-04 22:07:31 +0200

    element adapter created

--- app/src/main/java/com/example/themichalkozak/touristapp/ElementAdapter.java
+++ app/src/main/java/com/example/themichalkozak/touristapp/ElementAdapter.java
@@ -1,8 +1,57 @@
 package com.example.themichalkozak.touristapp;
 
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 /**
  * Created by themichalkozak on 04/05/2018.
  */
 
-public class ElementAdapter {
+public class ElementAdapter extends ArrayAdapter<ListElements> {
+
+    public ElementAdapter(Context context, ListElements[] elements) {
+        super(context, 0, elements);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+        View listItemView = convertView;
+        if(listItemView == null){
+            listItemView = LayoutInflater.from(getContext()).inflate(
+                    R.layout.list_item,parent,false);
+        }
+
+        ListElements currentElement = getItem(position);
+
+
+        TextView elementName = listItemView.findViewById(R.id.list_item_element_name);
+        elementName.setText(currentElement.getmName());
+
+        ImageView elementImage = listItemView.findViewById(R.id.list_item_image_view);
+        elementImage.setImageResource(currentElement.getmImageResourceId());
+
+        TextView elementDescription = listItemView.findViewById(R.id.list_item_description);
+        elementDescription.setText(currentElement.getmDescription());
+
+        TextView elementTelefon = listItemView.findViewById(R.id.list_item_telefon);
+        elementTelefon.setText(currentElement.getmTelefonNumber());
+
+        TextView elementWeb = listItemView.findViewById(R.id.list_item_web);
+        elementWeb.setText(currentElement.getmWeb());
+
+        TextView elementEmail = listItemView.findViewById(R.id.list_item_e_mail);
+        elementEmail.setText(currentElement.geteMail());
+        return listItemView;
+    }
+
+
 }
--- app/src/main/java/com/example/themichalkozak/touristapp/ListElements.java
+++ app/src/main/java/com/example/themichalkozak/touristapp/ListElements.java
@@ -6,5 +6,43 @@ package com.example.themichalkozak.touristapp;
 
 public class ListElements {
 
+    private String mName;
+    private int mImageResourceId;
+    private String mDescription;
+    private String mTelefonNumber;
+    private String mWeb;
+    private String eMail;
 
+    public ListElements(String mName, int mImage, String mDescription, String mTelefonNumber, String mWeb, String eMail) {
+        this.mName = mName;
+        this.mImageResourceId = mImage;
+        this.mDescription = mDescription;
+        this.mTelefonNumber = mTelefonNumber;
+        this.mWeb = mWeb;
+        this.eMail = eMail;
+    }
+
+    public String getmName() {
+        return mName;
+    }
+
+    public int getmImageResourceId() {
+        return mImageResourceId;
+    }
+
+    public String getmDescription() {
+        return mDescription;
+    }
+
+    public String getmTelefonNumber() {
+        return mTelefonNumber;
+    }
+
+    public String getmWeb() {
+        return mWeb;
+    }
+
+    public String geteMail() {
+        return eMail;
+    }
 }
